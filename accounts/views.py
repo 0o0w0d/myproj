1cf65fe71025924897e657118fdf1a6cc8575eb8
zion <0o0w0d@gmail.com>
2024-07-06 13:18:21 +0900

    (accounts) logout
    - htmx를 이용해 logout 구현
    - hx-push-url 속성을 이용해 새로운 콘텐츠 로드 시 브라우저의 URL 변경되도록 설정
    - TODO: 로그아웃 시 toast-message가 보이지 않는 현상 해결

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView as DjangoLoginView,
+    LogoutView as DjangoLogoutView,
+)
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import LoginForm
 
 
-# Create your views here.
 class LoginView(DjangoLoginView):
     form_class = LoginForm
     template_name = "crispy_form.html"
 
     # context 추가
     extra_context = {"form_title": "Login"}
+
+
+class LogoutView(DjangoLogoutView):
+    next_page = "accounts:login"
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.success(request, "logout successed :)")
+        return response
 
 
 @login_required
