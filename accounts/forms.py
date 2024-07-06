477221773b2aae2437c9bd4bf1b11d8ed3abc083
zion <0o0w0d@gmail.com>
2024-07-07 01:29:02 +0900

    convert profile images to jpeg format and reduce size
    - pillow 라이브러리를 이용해 직접 썸네일 처리
    - jpeg로 변환하고 이미지 크기 줄이기

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,13 @@
+import os
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 from django.forms import ValidationError
+from django.core.files import File
+from django.core.files.base import ContentFile
+from PIL import Image
 
 from .models import Profile, User
 
@@ -48,3 +52,19 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["avatar"]
+
+    def clean_avatar(self):
+        avatar_file: File = self.cleaned_data.get("avatar")
+        if avatar_file:
+            img = Image.open(avatar_file)
+            MAX_SIZE = (512, 512)
+            img.thumbnail(MAX_SIZE)
+            img = img.convert("RGB")
+
+            thumb_name = os.path.splitext(avatar_file.name)[0]
+            thumb_file = ContentFile(b"", name=thumb_name)
+            img.save(thumb_file, format="jpeg")
+
+            return thumb_file
+
+        return avatar_file
