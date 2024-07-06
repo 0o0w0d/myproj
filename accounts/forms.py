8689b45d9eef1e15501545b94aa01770c9dcfe98
zion <0o0w0d@gmail.com>
2024-07-06 22:49:54 +0900

    require email during signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
+from django.forms import ValidationError
 
 from .models import User
 
@@ -16,8 +17,21 @@ class LoginForm(AuthenticationForm):
 class SignUpForm(UserCreationForm):
     helper = FormHelper()
     helper.attrs = {"novalidate": True}
-    helper.layout = Layout("username", "password1", "password2")
+    helper.layout = Layout("username", "email", "password1", "password2")
     helper.add_input(Submit("submit", "Sign Up", css_class="w-100"))
 
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].required = True
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if email:
+            user = User.objects.filter(email__iexact=email)
+            if user.exists():
+                raise ValidationError("email already exists :(")
+        return email
