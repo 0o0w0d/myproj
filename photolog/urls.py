821e6237d008253fb4e5aabb723d51140f3b695e
zion <0o0w0d@gmail.com>
2024-07-09 00:16:25 +0900

    implement note edit page without photo removal feature
    - 사진 삭제 기능을 제외한 note edit 기본 구현

--- photolog/urls.py
+++ photolog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("new/", views.NoteCreateView.as_view(), name="note_new"),
     path("<int:pk>/", views.NoteDetailView.as_view(), name="note_detail"),
+    path("<int:pk>/edit/", views.note_edit, name="note_edit"),
 ]
--- photolog/views.py
+++ photolog/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import CreateView, DetailView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import CreateView, DetailView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 
 from .models import Note, Photo
@@ -39,3 +40,41 @@ class NoteCreateView(LoginRequiredMixin, CreateView):
 
 class NoteDetailView(DetailView):
     model = Note
+
+
+# TODO: photos에 대한 처리를 위해 Formset 이용 => CBV보다 FBV가 구현 용이
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Note
+    form_class = NoteForm
+    template_name = "crispy_form.html"
+    extra_context = {"form_title": "Edit Note"}
+
+    # Note.author에 한해서만 수정 가능하도록 queryset 설정
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(author=self.request.user)
+        return qs
+
+
+@login_required
+def note_edit(request, pk):
+    note = get_object_or_404(Note, pk=pk, author=request.user)
+
+    if request.method == "GET":
+        form = NoteForm(instance=note)
+    else:
+        form = NoteForm(data=request.POST, files=request.FILES, instance=note)
+        if form.is_valid():
+            saved_note = form.save()
+
+            # 모델에 포함되어있는 필드가 아니므로, 따로 저장 로직 구현
+            photo_file_list = form.cleaned_data.get("photos")
+            if photo_file_list:
+                Photo.create_photos(saved_note, photo_file_list)
+
+            messages.success(request, f"note #{saved_note.pk} saved! :)")
+            return redirect(saved_note)
+
+    return render(
+        request, "crispy_form.html", {"form": form, "form_title": "Note Edit"}
+    )
