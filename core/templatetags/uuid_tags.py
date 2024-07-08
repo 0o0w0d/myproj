e843e7abe1596c3666448f09f00a1f22b9ffd699
zion <0o0w0d@gmail.com>
2024-07-08 23:22:21 +0900

    add carousel component and apply note detail page
    - bootstrap5 carousel 이용
    - carousel.py에서 get_context_data로 image_url을 받아오기 위한 처리 구현
    - 여러 carousel이 사용될 상황을 대비해 unique한 carousel id 값을 위해 uuid_tags 커스텀 태그 구현

---
+++ core/templatetags/uuid_tags.py
@@ -0,0 +1,13 @@
+from django import template
+import uuid
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def generate_uuid4(prefix: str = None, length: int = None) -> str:
+    uuid4_hex = uuid.uuid4().hex
+    if length:
+        uuid4_hex = uuid4_hex[:length]
+    return (prefix or "") + uuid4_hex
