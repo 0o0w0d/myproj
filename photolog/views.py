02a9741186ff901106c05a6a1a3819e689edc205
zion <0o0w0d@gmail.com>
2024-07-10 17:24:38 +0900

    comment list in note detail
    - note detail 페이지에 comment list 템플릿 기본 구현

--- photolog/views.py
+++ photolog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms import BaseModelForm
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django_htmx.http import trigger_client_event
