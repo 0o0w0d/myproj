9f8e118bf8b37e02fc148556af60787d471d2cf0
zion <0o0w0d@gmail.com>
2024-07-12 13:09:18 +0900

    add following_set field in user.User

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,23 @@ from django.contrib.auth.models import AbstractUser
 
 # project begging step, add custom User(not use auth.User)
 class User(AbstractUser):
-    pass
+    following_set = models.ManyToManyField(
+        "self", related_name="follower_set", symmetrical=False, blank=True
+    )
+
+    # following 관계에 대한 메서드 추가
+    def is_follower(self, to_user) -> bool:
+        return self.following_set.filter(pk=to_user.pk).exists()
+
+    def follow(self, to_user) -> None:
+        return self.following_set.add(to_user)
+
+    def unfollowing(self, to_user) -> None:
+        return self.following_set.remove(to_user)
+
+    # 유저가 팔로우하고 있는 사람
+    def follower_count(self) -> int:
+        return self.follower_set.count()
 
 
 class Profile(models.Model):
