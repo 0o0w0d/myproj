b6ae7b0be91770e3afe131aeeb46f5e95a443b65
zion <0o0w0d@gmail.com>
2024-07-15 15:11:20 +0900

    Foreign Key lookup : __str__
    - Model의 __str__ 메서드를 사용하는 StringRelatedField => 문자열로 외래키 관계 조회

--- blog/serializers.py
+++ blog/serializers.py
@@ -11,12 +11,18 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class PostListSerializer(serializers.ModelSerializer):
+    # Model에서 __str__ 속성으로 지정된 값 반환
+    # AbstractUser default config : __str__ -> 'username'
+    author = serializers.StringRelatedField()
+
     class Meta:
         model = Post
         fields = ["id", "title", "author"]
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+
     class Meta:
         model = Post
         fields = ["id", "title", "content", "author"]
