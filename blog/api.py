c682fb2d033a9d1e8fd8f5cec82a7969223a6c6d
zion <0o0w0d@gmail.com>
2024-07-15 14:48:47 +0900

    single model API: different data structure using serializer
    - 다른 데이터 구조를 가져올 때, serializer도 따로 생성
    - 쿼리셋 조회 시 defer를 사용해 지연 로드 가능

--- blog/api.py
+++ blog/api.py
@@ -1,16 +1,17 @@
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, PostListSerializer, PostDetailSerializer
 from django.shortcuts import get_object_or_404
 
 from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
-    post_qs = Post.objects.all()
+    # 모든 Post 객체를 가져오지만 content 필드는 지연 로드
+    post_qs = Post.objects.all().defer("content")
 
     # list일 때 many 인자를 지정하지 않으면 모델 instance로 인식해 error
-    serializer = PostSerializer(instance=post_qs, many=True)
+    serializer = PostListSerializer(instance=post_qs, many=True)
     # serializer의 data 속성을 통해 python 기본 데이터 타입으로 변환된 값 참조
     list_data: ReturnList = serializer.data
 
@@ -20,7 +21,7 @@ def post_list(request: HttpRequest) -> HttpResponse:
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     post = get_object_or_404(Post, pk=pk)
 
-    serializer = PostSerializer(instance=post)
+    serializer = PostDetailSerializer(instance=post)
     detail_data: ReturnDict = serializer.data
 
     return JsonResponse(detail_data)
--- blog/serializers.py
+++ blog/serializers.py
@@ -8,3 +8,15 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["id", "title", "content"]
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", "title"]
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", "title", "content"]
