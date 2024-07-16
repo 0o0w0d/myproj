4d039c4799e8de8f8f527390d6dfd93cf66214c8
zion <0o0w0d@gmail.com>
2024-07-16 13:04:12 +0900

    DRF Generic APIView

--- blog/api.py
+++ blog/api.py
@@ -4,27 +4,52 @@ from .serializers import PostSerializer, PostListSerializer, PostDetailSerialize
 from django.shortcuts import get_object_or_404
 
 from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 # api.py ~= views.py
 
-
-def post_list(request: HttpRequest) -> HttpResponse:
-    # 모든 Post 객체를 가져오지만 content 필드는 지연 로드
-    # N + 1 문제 해결을 위해 author 필드에 대한 즉시 로딩 수행
-    post_qs = Post.objects.all().defer("content").select_related("author")
-
-    # list일 때 many 인자를 지정하지 않으면 모델 instance로 인식해 error
-    serializer = PostListSerializer(instance=post_qs, many=True)
-    # serializer의 data 속성을 통해 python 기본 데이터 타입으로 변환된 값 참조
-    list_data: ReturnList = serializer.data
-
-    return JsonResponse(list_data, safe=False)
+# api_view 사용 시 rest_framework의 Response, Resquest를 사용
 
 
-def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    post = get_object_or_404(Post, pk=pk)
+# @api_view(["GET"])
+# def post_list(request: Request) -> Response:
+#     # 모든 Post 객체를 가져오지만 content 필드는 지연 로드
+#     # N + 1 문제 해결을 위해 author 필드에 대한 즉시 로딩 수행
+#     post_qs = Post.objects.all().defer("content").select_related("author")
 
-    serializer = PostDetailSerializer(instance=post)
-    detail_data: ReturnDict = serializer.data
+#     # list일 때 many 인자를 지정하지 않으면 모델 instance로 인식해 error
+#     serializer = PostListSerializer(instance=post_qs, many=True)
+#     # serializer의 data 속성을 통해 python 기본 데이터 타입으로 변환된 값 참조
+#     list_data: ReturnList = serializer.data
 
-    return JsonResponse(detail_data)
+#     return Response(list_data)
+
+
+# django generic class 기반으로 변경
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all().defer("content").select_related("author")
+    serializer_class = PostListSerializer
+
+
+post_list = PostListAPIView.as_view()
+
+
+# @api_view(["GET"])
+# def post_detail(request: Request, pk: int) -> Response:
+#     post = get_object_or_404(Post, pk=pk)
+
+#     serializer = PostDetailSerializer(instance=post)
+#     detail_data: ReturnDict = serializer.data
+
+#     return Response(detail_data)
+
+
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+
+
+post_detail = PostRetrieveAPIView.as_view()
