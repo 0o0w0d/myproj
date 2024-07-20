18c2ab50c303113209843aeda629f70a59913f7c
zion <0o0w0d@gmail.com>
2024-07-21 01:02:43 +0900

    example : sessionid cookie auto-sending with JavaScript fetch API
    - HTTP 요청을 보내는 웹 페이지의 프로토콜/호스트/포트가 같을 경우에만 요청 헤더에 Cookie 자동 전송

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 from . import api
 from .api import PostModelViewSet
 from rest_framework.routers import DefaultRouter
+from django.views.generic import TemplateView
 
 
 app_name = "blog"
@@ -11,7 +12,10 @@ router = DefaultRouter()
 router.register(prefix="posts", viewset=PostModelViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="blog/index.html"), name="index"),
+    path("whoami/", views.whoami, name="whoami"),
+]
 urlpatterns_api_v1 = []
 urlpatterns_api_v1 += router.urls
 
