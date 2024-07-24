376c1fc61c7baec4de7bac058994533a1f47017b
zion <0o0w0d@gmail.com>
2024-07-24 23:11:24 +0900

    receive view response in next.js
    - render_nextjs_page로 next.js 서버로 요청을 보내고 그 응답으로 뷰 응답
    - CSR 방식 구현

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django_nextjs.render import render_nextjs_page
 
 
 def whoami(request):
     status = 200 if request.user.is_authenticated else 401
     username = request.user.username or "anonymous"
     return HttpResponse(f"Your username is <strong>{username}</strong>.", status=status)
+
+
+async def index(request):
+    # return render(request, "blog/index.html")
+    # next.js 서버로 요청을 보내어 그 응답으로 뷰 응답
+    return await render_nextjs_page(request)
